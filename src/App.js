acc79dfec3e0f1f2d9575279c32618b9a30c91d9
daryagolubewa <darya.golubewa@gmail.com>
2018-05-27 15:16:58 +0300

    css styles are added;

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import {Button, ButtonToolbar, ButtonGroup, Row, Col, Grid, md, lg} from "react-bootstrap";
+
 
 const CURRENCIES = [
     {name: "Bitcoin", id: "bitcoin"},
@@ -22,28 +23,40 @@ class App extends Component {
             activeCurrency: 0,
         }
     }
-  render() {
-      const activeCurrency = this.state.activeCurrency;
-    return (
-      <div className="App">
-          {CURRENCIES.map((currency, index) => (
-              <button
-              key={index}
-              onClick={() => {
-                  this.setState({activeCurrency: index});
-              }}
-              >
-                  {currency.name}
-              </button>
-          ) )}
-          <CurrencyDisplay
-              key={activeCurrency}
-              id={CURRENCIES[activeCurrency].id}
-              name={CURRENCIES[activeCurrency].name}
-          />
-      </div>
-    );
-  }
+    render() {
+        const activeCurrency = this.state.activeCurrency;
+        return (
+            <div className="App" >
+                <Grid>
+                    <Row>
+                        <Col md={12}>
+                            <ButtonGroup>
+                            {CURRENCIES.map((currency, index) => (
+                                <Button  bsStyle="info" bsSize="small"
+                                         key={index}
+                                         onClick={() => {
+                                             this.setState({activeCurrency: index});
+                                         }}
+                                >
+                                    {currency.name}
+                                </Button>
+                            ) )}
+                            </ButtonGroup>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={8} mdOffset={2}>
+                            <CurrencyDisplay
+                                key={activeCurrency}
+                                id={CURRENCIES[activeCurrency].id}
+                                name={CURRENCIES[activeCurrency].name}
+                            />
+                        </Col>
+                    </Row>
+                </Grid>
+            </div>
+        );
+    }
 }
 
 export default App;
@@ -70,10 +83,10 @@ class CurrencyDisplay extends Component {
                 <h1>
                     Текущий курс {currencyData[0].name} составляет:
                 </h1>
-                <p>Цена в долларах за штуку: {currencyData[0].percent_change_1h}</p>
+                <p>Цена в долларах за штуку: {currencyData[0].price_usd}</p>
                 <p>Цена в биткоинах за штуку: {currencyData[0].price_btc} </p>
                 <p>Общая стоимость валюты в долларах: {currencyData[0].market_cap_usd}</p>
-                <p>Изменение курса за 1 час (%):  {currencyData[0].percent_change_1h}</p>
+                <p>Изменение курса за 1 час (%): {currencyData[0].percent_change_1h}</p>
                 <p>Изменение курса за 1 день (%): {currencyData[0].percent_change_24h}</p>
                 <p>Изменение курса за неделю (%): {currencyData[0].percent_change_7d}</p>
             </div>
